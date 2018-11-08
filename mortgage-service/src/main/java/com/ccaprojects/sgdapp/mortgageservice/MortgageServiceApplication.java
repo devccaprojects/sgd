ccd2bae0658c8ab4421664388c451583b94f660d
Christian Luque Vera <luqueve@gmail.com>
2018-11-07 22:03:12 -0500

    Add client-service, contract-service and mortgage-service

---
+++ mortgage-service/src/main/java/com/ccaprojects/sgdapp/mortgageservice/MortgageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ccaprojects.sgdapp.mortgageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageServiceApplication.class, args);
+	}
+}
